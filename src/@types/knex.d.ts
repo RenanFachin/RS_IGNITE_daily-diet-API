2a5a59c1c5c3463ad91b08d8e698c40063e16040
Renan <renan.fachin@hotmail.com>
2023-04-19 18:06:23 -0300

    Adiciona tipagem às tabelas

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,24 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    meals: {
+      id: string
+      user_id: string
+      name: string
+      description: string
+      isOnTheDiet: boolean
+      created_at: string
+      session_id?: string
+    }
+    users: {
+      id: string
+      name: string
+      email: string
+      address: string
+      weight: number
+      height: number
+    }
+  }
+}
