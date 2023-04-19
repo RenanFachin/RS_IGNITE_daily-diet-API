75e43d27042b4a5a3a9a5dfa32ab5b62c015ef0c
Renan <renan.fachin@hotmail.com>
2023-04-19 16:50:29 -0300

    Cria rota para criar nova refeição

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,8 +1,28 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import crypto from 'node:crypto'
+import { z } from 'zod'
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.post('/', async (request, response) => {
-    return response.send()
+    const createMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      isOnTheDiet: z.boolean(),
+    })
+
+    const { name, description, isOnTheDiet } = createMealBodySchema.parse(
+      request.body,
+    )
+
+    await knex('meals').insert({
+      id: crypto.randomUUID(),
+      user_id: crypto.randomUUID(), // gerando um id aleatório de user por enquanto
+      name,
+      description,
+      isOnTheDiet,
+    })
+
+    return response.status(201).send()
   })
 }
